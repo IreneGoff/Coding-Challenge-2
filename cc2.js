153de1f4b546c08fb95df4a56a5df269553d15aa
Irene Goff <ireneegoff@gmail.com>
2024-05-21 18:11:31 -0400

    Implement and populate arrays for bills, tips, and totals

--- cc2.js
+++ cc2.js
@@ -11,3 +11,21 @@ let testBill = 100;
 let testTip = calcTip(testBill);
 console.log(`For a bill of $${testBill}, the tip is $${testTip}`);
 
+let bills = [275, 40, 430];
+let tips = bills.map(calcTip);
+let totals = bills.map((bill, index) => bill + tips[index]);
+
+console.log("Data Set 1:");
+console.log("Bills:", bills);
+console.log("Tips:", tips);
+console.log("Totals:", totals);
+
+bills = [125, 555, 44];
+tips = bills.map(calcTip);
+totals = bills.map((bill, index) => bill + tips[index]);
+
+console.log("Data Set 2:");
+console.log("Bills:", bills);
+console.log("Tips:", tips);
+console.log("Totals:", totals);
+
