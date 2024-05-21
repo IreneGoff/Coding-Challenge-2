eb1c957358f33af51910d32214a95b221193ab26
Irene Goff <ireneegoff@gmail.com>
2024-05-21 18:10:57 -0400

    Create and test calcTip function

--- cc2.js
+++ cc2.js
@@ -4,3 +4,10 @@ console.log(tip);
 
 console.log(`The bill was $${bill}, the tip was $${tip}, and the total value $${bill + tip}`);
 
+function calcTip(bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+let testBill = 100;
+let testTip = calcTip(testBill);
+console.log(`For a bill of $${testBill}, the tip is $${testTip}`);
+
