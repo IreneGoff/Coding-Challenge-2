69ae185c204fa0b176ade5111cbd53d0c3e214fb
Irene Goff <ireneegoff@gmail.com>
2024-05-21 18:10:18 -0400

    Add output functionality to display bill, tip, and total

--- cc2.js
+++ cc2.js
@@ -2,3 +2,5 @@ let bill = 275;
 let tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 console.log(tip);
 
+console.log(`The bill was $${bill}, the tip was $${tip}, and the total value $${bill + tip}`);
+
